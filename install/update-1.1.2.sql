413ca67f3036123c1d64afb05d52392c231d3dc7
Atik <atiklabs@pm.me>
2018-07-19 17:28:17 +0300

    1.1.2 Added salud cateogory

---
+++ install/update-1.1.2.sql
@@ -0,0 +1,13 @@
+--
+-- Foonances project
+--
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Categories entries
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+  (8, 'Salud');
